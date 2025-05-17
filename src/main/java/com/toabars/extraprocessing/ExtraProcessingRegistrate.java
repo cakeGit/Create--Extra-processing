966866d8979d4dfcbd71bccc0902a72fda92a233
Arthur <tobara.santos09@gmail.com>
2025-05-17 19:38:58 +0900

    Still a valid mod file

--- src/main/java/com/toabars/extraprocessing/ExtraProcessingRegistrate.java
+++ src/main/java/com/toabars/extraprocessing/ExtraProcessingRegistrate.java
@@ -2,12 +2,17 @@ package com.toabars.extraprocessing;
 
 import com.toabars.extraprocessing.content.shitblock.ShitBlock;
 import com.tterrag.registrate.util.entry.BlockEntry;
+import com.tterrag.registrate.util.entry.ItemEntry;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 
 public class ExtraProcessingRegistrate {
     public static BlockEntry<Block> SHIT_BLOCK = CreateExtraprocessing.REGISTRATE.block("shit_block", Block::new)
             .defaultBlockstate()
             .simpleItem()
+            .register();
+
+    public static ItemEntry<Item> SHIT_ITEM = CreateExtraprocessing.REGISTRATE.item("shit", Item::new)
             .register();
 
     public static void register() {
